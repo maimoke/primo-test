c0f2d261464f9eddf2ef3ae109e67c47ff7f2dda
Chayanin Liengjindathaworn <maimokemm@hotmail.com>
2025-12-20 00:32:57 +0700

    init project

---
+++ build/index.d.ts
@@ -0,0 +1,3 @@
+export declare function hello(who?: string): string;
+export declare function merge(collection_1: number[], collection_2: number[], collection_3: number[]): number[];
+//# sourceMappingURL=index.d.ts.map
---
+++ build/index.js
@@ -0,0 +1,75 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = hello;
+exports.merge = merge;
+const world = "world";
+const col1 = [1, 5, 7]; //min to max
+const col2 = [8, 6, 3, 2]; //max to min
+const col3 = [0, 4, 9, 2]; //min to max
+function hello(who = world) {
+    return `Hello ${who}!`;
+}
+function merge(collection_1, collection_2, collection_3) {
+    collection_2.reverse();
+    var sorted = [];
+    //sort
+    var numCol1 = collection_1.pop();
+    if (numCol1 === undefined) {
+        numCol1 = -1;
+    }
+    var numCol2 = collection_2.pop();
+    if (numCol2 === undefined) {
+        numCol2 = -1;
+    }
+    var numCol3 = collection_3.pop();
+    if (numCol3 === undefined) {
+        numCol3 = -1;
+    }
+    //pick next number
+    while (true) {
+        // console.log(numCol1)
+        // console.log(numCol2)
+        // console.log(numCol3)
+        if (numCol1 == -1 && numCol2 == -1 && numCol3 == -1) {
+            break;
+        }
+        let push = 0;
+        if (numCol1 > numCol2 && numCol1 > numCol3) {
+            push = 1;
+        }
+        else if (numCol2 > numCol1 && numCol2 > numCol3) {
+            push = 2;
+        }
+        else if (numCol3 > numCol1 && numCol3 > numCol1) {
+            push = 3;
+        }
+        else {
+            break;
+        }
+        if (push == 1) {
+            sorted.push(numCol1);
+            numCol1 = collection_1.pop();
+            if (numCol1 === undefined) {
+                numCol1 = -1;
+            }
+        }
+        else if (push == 2) {
+            sorted.push(numCol2);
+            numCol2 = collection_2.pop();
+            if (numCol2 === undefined) {
+                numCol2 = -1;
+            }
+        }
+        else if (push == 3) {
+            sorted.push(numCol3);
+            numCol3 = collection_3.pop();
+            if (numCol3 === undefined) {
+                numCol3 = -1;
+            }
+        }
+        // console.log(sorted)
+    }
+    return sorted.reverse();
+}
+console.log(merge(col1, col2, col3));
+//# sourceMappingURL=index.js.map
